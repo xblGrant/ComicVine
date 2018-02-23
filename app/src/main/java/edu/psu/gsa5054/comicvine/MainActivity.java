bb026faca7b92ac467617d9e2b7f1a72ee2a6241
Grant Abbondanza <35235145+xblGrant@users.noreply.github.com>
2018-02-22 22:46:49 -0500

    Initial commit

---
+++ app/src/main/java/edu/psu/gsa5054/comicvine/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.psu.gsa5054.comicvine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/edu/psu/gsa5054/comicvine/SearchScreen.java
@@ -0,0 +1,25 @@
+package edu.psu.gsa5054.comicvine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+
+public class SearchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.search_screen_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+}
